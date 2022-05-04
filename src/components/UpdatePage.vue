bcb37c61aa6be68e563c0b2ccff5c141d07a9f3c
usc-kmr041 <81274276+usc-kmr041@users.noreply.github.com>
2022-05-04 16:28:33 +1000

    update

---
+++ src/components/UpdatePage.vue
@@ -0,0 +1,21 @@
+<template>
+<HeaderMenu />
+<h1>update page!</h1>
+</template>
+
+<script>
+import HeaderMenu from './HeaderMenu.vue'
+export default {
+    name:'UpdatePage',
+    components: {
+        HeaderMenu
+    },
+        mounted(){
+        let user = localStorage.getItem('userinfo');
+        if(!user)
+        {
+            this.$router.push({name:"SignUp"})
+        }
+    }
+}
+</script>
